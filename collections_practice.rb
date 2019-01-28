0ba15faf767f71950fff8031fd056b13eb538e87
Joshua M Loran <joshua.loran@gmail.com>
2019-01-28 17:42:14 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,52 @@
-# your code goes here
+def contain_a(array)
+  new_array = []
+  i = 0
+  while i < array.count
+    if array[i].include?("a")
+      new_array << array[i]
+    end
+    i += 1
+  end
+  new_array
+end
+
+def begins_with_r(array)
+  i = 0
+  bool = true
+  while i < array.count
+    if !array[i].start_with?("r")
+      bool = false
+    end
+    i += 1
+  end
+  bool
+end
+
+def first_wa(array)
+  i = 0
+  result = ""
+  while i < array.count
+    if array[i].include?("wa")
+      result = array[i]
+    end
+    i += 1
+  end
+  result
+end
+
+def remove_non_strings(array)
+  array.delete_if { |obj| !(obj.is_a? String)}
+end
+
+def count_elements(array)
+  i = 0
+  repeats = 0
+  name = "blake"
+  while i < array.count
+    if array[i] == name
+      repeats += 1
+    end
+    i += 1
+  end
+  repeats
+end

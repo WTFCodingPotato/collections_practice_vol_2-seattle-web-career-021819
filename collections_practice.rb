89fb5bf4e8e493790b01f89b8c13498f0fb8536e
Joshua M Loran <joshua.loran@gmail.com>
2019-01-28 18:11:52 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -37,7 +37,7 @@ end
 def count_elements(array)
     new_hash = Hash.new(0)
     array.each do |element|
-      new_hash[element][:count] += 1
+      new_hash[element] += 1
     end
     new_hash
 end
